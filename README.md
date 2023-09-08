bcbf7e16e3fc031da32275216660a48b3afb4670
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2023-09-08 10:19:14 +0200

    first commit yahooooo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # oslo-npm2
+
 this is the project that will override the other one
+
+## very cool
---
+++ script.js
@@ -0,0 +1,5 @@
+const isBergen = (string) => {
+    return string === "Bergen"
+}
+
+module.exports = isBergen
